489efdb135293a85ac0e92e2b26edcdd387f42bc
Loice-vanessa <icyezavanessa@gmail.com>
2022-11-21 11:13:27 +0200

    node-crone & sendMails

--- src/controller/teamController.js
+++ src/controller/teamController.js
@@ -1,6 +1,34 @@
 import teamModel from "../models/teamModel";
 import handleCRUD from "../utils/handleCRUD";
-const createteam = handleCRUD.creatOne(teamModel);
+
+const cron = require('node-cron');
+
+
+const settings = {
+    MINUTE:"* * * * *"
+}
+
+
+export const createteam =  async(req,res,next)=> {
+    
+    
+    try{
+        const doc  = await teamModel.create(req.body);
+        if(!doc){
+            return res.status(400).json({message:"failed to register"});
+        }
+
+        cron.schedule(settings[req.body.settings], () => {
+            console.log('running a task every minute');
+        });
+
+        return res.status(201).json({message:"sucessfully", data:doc});
+    }
+    catch (error){
+        console.log(error)
+
+    }
+}
 const getAllteam = handleCRUD.getAll(teamModel);
 const UpdateOneteam = handleCRUD.updateOneById (teamModel);
 const getOneteam = handleCRUD.getOneById (teamModel);
