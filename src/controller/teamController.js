a4d57cdce25fcced91987890b6dcd2a1934931a4
Loice-vanessa <icyezavanessa@gmail.com>
2022-11-25 11:49:17 +0200

    join team

--- src/controller/teamController.js
+++ src/controller/teamController.js
@@ -4,9 +4,10 @@ import Participant from "../models/participants";
 
 import user from '../models/userModel';
 import { userIdFromToken } from '../utils/token';
+import jwt from 'jsonwebtoken';
 
 const cron = require('node-cron');
-const express = require('express')
+
 
 const settings = {
     MINUTE:"* * * * * ",
@@ -15,10 +16,7 @@ const settings = {
     Month:"* * */1 * *",
     every:"0 8 */1 */1 1,6",
 }
-const api = express()
 
-
-// const teamId = teamIdFromToken(req.header('x-auth-token'));
 export const createteam =  async(req,res,next)=> {
 
 
@@ -26,10 +24,6 @@ export const createteam =  async(req,res,next)=> {
 
     
     try{
-
-
-
-
 
 
         req.body.TeamAdmin=req.userId;
@@ -63,27 +57,6 @@ const getAll= await Participant.find({UserId:req.userId}).populate("TeamId","Tea
 return res.status(200).send(getAll);
 }
 
-
-const matchingTeam = async (req, res) => {
-    try {
-      const teamId = teamIdFromToken(req.header('x-auth-token'));
-      const team = await Team.findById(teamId);
-      if (team) {
-        const team = await team.find({
-          $or: [ { 'TeamName.first': user.first },  ],
-        });
-        if (team.length) {
-          const sortedTeam = team.sort((a, b) => (new Date(b.teamcreatedat)).getTime()
-            - (new Date(a.teamcreatedat).getTime()));
-          return response.successResponse(res, 200, 'team matching your data', sortedTeam);
-        }
-        return response.errorResponse(res, 404, 'team matches with your data are not available');
-      }
-      return response.errorResponse(res, 401, 'You are not a user');
-    } catch (error) {
-      return response.errorResponse(res, 500, error);
-    }
-  };
 
 
 const findALLteam = async (req, res) => {
--- src/routes/teamRouter.js
+++ src/routes/teamRouter.js
@@ -3,10 +3,15 @@ import checkToken from "../middleWares/checkToken";
 import teamController from "../controller/teamController";
 
 const router =  Router();
-router.post('/',checkToken,teamController.createteam);
-router.get('/',checkToken,teamController.getAllteam);
+
+router.post('/',teamController.createteam);
+router.get('/',teamController.getAllteam);
 router.patch('/:id',teamController.UpdateOneteam);
 router.delete('/:id',teamController.deleteOneteam);
 router.get('/:id',teamController.getOneteam);
-router.get('/join/:id',checkToken,teamController.joinTeam);
+// router.get('/join/:id',teamController.getOneteam);
+router.get('/join/:teamId',function(req, res){
+    res.send('you requested to see a team with the id of ' + req.params.teamId)
+});
 export default router;
+
