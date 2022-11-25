5e549fb785dfc5f5e37d3d330b03167fb24f94a9
Loice-vanessa <icyezavanessa@gmail.com>
2022-11-25 12:17:00 +0200

    join team

--- src/routes/teamRouter.js
+++ src/routes/teamRouter.js
@@ -5,13 +5,10 @@ import teamController from "../controller/teamController";
 const router =  Router();
 
 router.post('/',teamController.createteam);
-router.get('/',teamController.getAllteam);
+router.get('/',checkToken,teamController.getAllteam);
 router.patch('/:id',teamController.UpdateOneteam);
 router.delete('/:id',teamController.deleteOneteam);
 router.get('/:id',teamController.getOneteam);
-// router.get('/join/:id',teamController.getOneteam);
-router.get('/join/:teamId',function(req, res){
-    res.send('you requested to see a team with the id of ' + req.params.teamId)
-});
+router.get('/join/:id',checkToken,teamController.joinTeam);
 export default router;
 
