c4445e3728437986497368a8b85e7b796119a70d
Vicky <luandavicky5@gmail.com>
2022-12-07 12:26:57 +0200

    all

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,6 +12,7 @@ const router = Router();
  router.use("/api/password-reset",passwordReset)
 router.use('/user',userRouter);
 router.use('/team',teamRouter);
+router.use('/teams',teamRouter);
 router.use('/type',teamtypeRouter);
 router.use('/wallet',walletRouter);
 
