bb1db87ed8691d3fa1494633d7915c542b6e6ec4
Loice-vanessa <icyezavanessa@gmail.com>
2022-12-08 12:05:17 +0200

    join team

--- src/routes/teamRouter.js
+++ src/routes/teamRouter.js
@@ -1,17 +1,14 @@
-// 
 import { Router } from "express";
 import checkToken from "../middleWares/checkToken";
 import teamController from "../controller/teamController";
+
 const router =  Router();
-router.post('/',checkToken,teamController.createteam);
+
+router.post('/',teamController.createteam);
 router.get('/',checkToken,teamController.getAllteam);
 router.patch('/:id',teamController.UpdateOneteam);
 router.delete('/:id',teamController.deleteOneteam);
 router.get('/:id',teamController.getOneteam);
-router.get('/contributions/:id',teamController.getAllContributions)
-router.put('/contributions/:id',teamController.updateContribution)
-
-
-
-
+router.get('/join/:id',checkToken,teamController.joinTeam);
 export default router;
+
